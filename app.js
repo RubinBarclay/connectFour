0d48e4759d07eec15b6022ee417510ce0bda8ee6
Rubin Barclay <rubinbarclay@protonmail.com>
2020-10-18 15:08:09 +0200

    last diagonal win condition done

--- app.js
+++ app.js
@@ -57,7 +57,12 @@ function winConditions(slot, curPlayer) {
   // console.log(col);
 
 
+  // Will hold loop count
   let loopCount;
+  // Will determine diagonal loop count
+  let singleLoop;
+  let doubleLoop;
+  let tripleLoop;
 
   // HORIZONTAL : the win pattern check shifts to the right each loop
   // i <= Math.abs(....) is how many possible win patterns there are i.e: loop count
@@ -108,10 +113,10 @@ function winConditions(slot, curPlayer) {
 
   // DIAGONAL /
   
-  //loopCount arrays 
-  let singleLoop = ['r4c1', 'r3c2', 'r2c3', 'r1c4', 'r6c4', 'r5c5', 'r4c6', 'r3c7', 'r5c1', 'r6c1', 'r6c2', 'r6c3', 'r1c5', 'r1c6', 'r1c7', 'r2c7'];
-  let doubleLoop = ['r5c1', 'r4c2', 'r3c3', 'r2c4', 'r1c5', 'r6c3', 'r5c4', 'r4c5', 'r3c6', 'r2c7', 'r5c2', 'r2c5', 'r5c3', 'r2c6'];
-  let tripleLoop = ['r4c3', 'r3c4', 'r4c4', 'r3c5'];
+  // slotID determines loopCount
+  singleLoop = ['r4c1', 'r3c2', 'r2c3', 'r1c4', 'r6c4', 'r5c5', 'r4c6', 'r3c7', 'r5c1', 'r6c1', 'r6c2', 'r6c3', 'r1c5', 'r1c6', 'r1c7', 'r2c7'];
+  doubleLoop = ['r5c1', 'r4c2', 'r3c3', 'r2c4', 'r1c5', 'r6c3', 'r5c4', 'r4c5', 'r3c6', 'r2c7', 'r5c2', 'r2c5', 'r5c3', 'r2c6'];
+  tripleLoop = ['r4c3', 'r3c4', 'r4c4', 'r3c5'];
   
   
     // max 3 (1, 2 or 3)
@@ -120,9 +125,8 @@ function winConditions(slot, curPlayer) {
               : tripleLoop.indexOf(slot) > - 1 ? 3 // loop thrice
               : 0 // no possible win pattern
 
-    console.log(loopCount);
     for (let i = 0; i < loopCount; i++) {
-      
+      /* 
       console.log([
         (col - (3 - i) >= 0 && row - (3 - i) >= 0)
         ? board[col - (3 - i)][row - (3 - i)] 
@@ -137,43 +141,75 @@ function winConditions(slot, curPlayer) {
         ? board[col - (0 - i)][row - (0 - i)] 
         : board[col][0]
       ]);
-
+      */
       if ([ 
         (col - (3 - i) >= 0 && row - (3 - i) >= 0)
         ? board[col - (3 - i)][row - (3 - i)] 
-        : board[col + 1][3],
+        : board[col + 1][row + 1],
         (col - (2 - i) >= 0 && row - (2 - i) >= 0)
         ? board[col - (2 - i)][row - (2 - i)] 
-        : board[col + 2][2],
+        : board[col + 2][row + 2],
         (col - (1 - i) >= 0 && row - (1 - i) >= 0)
         ? board[col - (1 - i)][row - (1 - i)] 
-        : board[col + 3][1],
+        : board[col + 3][row + 3],
         (col - (0 - i) >= 0 && row - (0 - i) >= 0)
         ? board[col - (0 - i)][row - (0 - i)] 
-        : board[col][0]
+        : board[col][row]
       ].every((val) => val === curPlayer)
       ) {
         return "Winner!";
       }
+    }
+
+  // DIAGONAL \
+  
+  // slotID determines loopCount
+  singleLoop = ['r1c1', 'r1c2', 'r1c3', 'r1c4', 'r2c1', 'r3c1', 'r4c2', 'r5c3', 'r6c4', 'r6c5', 'r6c6', 'r6c7', 'r5c7', 'r4c7', 'r3c6', 'r2c5'];
+  doubleLoop = ['r2c2', 'r2c3', 'r2c4', 'r3c2', 'r3c5', 'r4c3', 'r5c4', 'r5c5', 'r5c6', 'r4c6'];
+  tripleLoop = ['r3c3', 'r3c4', 'r4c4', 'r4c5'];
+  
+  
+    // max 3 (1, 2 or 3)
+    loopCount = singleLoop.indexOf(slot) > - 1 ? 1 // loop once
+              : doubleLoop.indexOf(slot) > - 1 ? 2 // loop twice
+              : tripleLoop.indexOf(slot) > - 1 ? 3 // loop thrice
+              : 0 // no possible win pattern
+
+    // console.log(loopCount);
+    for (let i = 0; i < loopCount; i++) {
       /*
-      if ([
-        (col - (3 - i) >= 0 && row - (3 - i) >= 0)
-        ? board[col - (3 - i)][row - (3 - i)] 
-        : board[3][3],
-        (col - (2 - i) >= 0 && row - (2 - i) >= 0)
-        ? board[col - (2 - i)][row - (2 - i)] 
-        : board[2][2],
-        (col - (1 - i) >= 0 && row - (1 - i) >= 0)
-        ? board[col - (1 - i)][row - (1 - i)] 
-        : board[1][1],
-        (col - (0 - i) >= 0 && row - (0 - i) >= 0)
-        ? board[col - (0 - i)][row - (0 - i)] 
-        : board[0][0]
+      console.log([
+        (col - (3 - i) >= 0 && row + (3 - i) <= 5)
+        ? board[col - (3 - i)][row + (3 - i)] 
+        : board[col + 1][row - 1],
+        (col - (2 - i) >= 0 && row + (2 - i) <= 5)
+        ? board[col - (2 - i)][row + (2 - i)] 
+        : board[col + 2][row - 2],
+        (col - (1 - i) >= 0 && row + (1 - i) <= 5)
+        ? board[col - (1 - i)][row + (1 - i)] 
+        : board[col + 3][row - 3],
+        (col - (0 - i) >= 0 && row + (0 - i) <= 5)
+        ? board[col - (0 - i)][row + (0 - i)] 
+        : board[col][row]
+      ]);
+      */
+      if ([ 
+        (col - (3 - i) >= 0 && row + (3 - i) <= 5)
+        ? board[col - (3 - i)][row + (3 - i)] 
+        : board[col + 1][row - 1],
+        (col - (2 - i) >= 0 && row + (2 - i) <= 5)
+        ? board[col - (2 - i)][row + (2 - i)] 
+        : board[col + 2][row - 2],
+        (col - (1 - i) >= 0 && row + (1 - i) <= 5)
+        ? board[col - (1 - i)][row + (1 - i)] 
+        : board[col + 3][row - 3],
+        (col - (0 - i) >= 0 && row + (0 - i) <= 5)
+        ? board[col - (0 - i)][row + (0 - i)] 
+        : board[col][row]
       ].every((val) => val === curPlayer)
       ) {
         return "Winner!";
       }
-      */
     }
     
   // } 
@@ -181,22 +217,6 @@ function winConditions(slot, curPlayer) {
   // No match found <-- is this necessary??????
   return null;
 }
-
-/*
-  // DIAGONAL \
-  loopCount = 0;
-  for (let i = 0; i <= loopCount; i++) {
-    
-    
-    if ([
-      
-    ].every((val) => val === curPlayer)
-    )
-  }
-  // No match found <-- is this necessary??????
-  return null;
-}
-*/
 
 // GLOBAL VARIABLES
 let player = 1;
